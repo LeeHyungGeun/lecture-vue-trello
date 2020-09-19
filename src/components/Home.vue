b78b17a609838754c117f7aefb39b211bf212b98
ken.lee <ken.lee@linecorp.com>
2020-09-19 16:56:03 +0900

    Add a Board

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     Home
+    <div>
+      <ul>
+        <li>
+          <router-link to="/b/1">Board 1</router-link>
+        </li>
+        <li>
+          <router-link to="/b/2">Board 2</router-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
